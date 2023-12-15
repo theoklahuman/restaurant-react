d4bc17ea7bb980632b512b8994c5cabdbadcb13d
Matt Oklahuman <theoklahuman@gmail.com>
2023-12-15 13:17:38 -0600

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Navbar from './Navbar'
+import './App.css'
+
+function App() {
+
+  return (
+    <Navbar />
+  )
+}
+
+export default App
---
+++ src/Navbar.jsx
@@ -0,0 +1,88 @@
+function Navbar() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            <img src="images/mcd.jpg" alt="logo" width="30" height="24" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a
+                  className="nav-link active"
+                  aria-current="page"
+                  href="/"
+                  id="homepage__tab"
+                >
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  id="donations-page"
+                  className="nav-link active"
+                  aria-current="page"
+                  href="#"
+                  data-bs-toggle="modal"
+                  data-bs-target=".modal"
+                >
+                  Request A Donation
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link active"
+                  id="careers-page__tab"
+                  aria-current="page"
+                  href="/careers"
+                >
+                  Careers
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link active"
+                  aria-current="page"
+                  href="/contact"
+                  id="contact-us__tab"
+                >
+                  Contact Us
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  data-bs-toggle="dropdown"
+                  href="#"
+                  role="button"
+                  aria-expanded="false"
+                >
+                  My Account
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
